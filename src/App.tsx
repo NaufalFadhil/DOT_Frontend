8073607ad1ac2ba74add05506f72e080cd0b6dae
Naufal Fadhil Athallah <naufalfadhilathallah@gmail.com>
2024-06-13 15:31:37 +0700

    feat(app): create routing on app

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,46 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import LoginPage from './pages/LoginPage';
+import { Route, Routes } from 'react-router-dom';
+import ProfilePage from './pages/ProfilePage';
+import NotFoundPage from './pages/NotFoundPage';
+import AuthContext from './context/AuthContext';
 
 function App() {
+  const [token, setToken] = React.useState<string | null>(null);
+
+  React.useEffect(() => {
+    const token = localStorage.getItem('token');
+    if (token) {
+      setToken(token);
+    }
+  }, []);
+  
+  const contextValue = React.useMemo(() => {
+    return {
+      token,
+      setToken
+    }
+  }, [token]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AuthContext.Provider value={contextValue}>
+      <div className="App">
+        <main>
+          {(token === null) ? (
+            <Routes>
+              <Route path="/*" element={<LoginPage />} />
+            </Routes>
+          ) : (
+            <Routes>
+              <Route path="/" element={<ProfilePage />} />
+              <Route path="/profiles" element={<ProfilePage />} />
+              <Route path='*' element={<NotFoundPage />} />
+            </Routes>
+          )}
+        </main>
+      </div>
+    </AuthContext.Provider>
   );
 }
 
